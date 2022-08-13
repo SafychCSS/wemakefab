8e1e31b1f530f63b1f8db4c425f672b199dbd2f3
safych <rs@leads.su>
2022-08-13 16:07:56 +0300

    add font, sass

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-import { createApp } from 'vue';
+import {createApp} from 'vue';
 import App from '@/App.vue';
 import store from '@/store';
+
+import '@/assets/scss/index.scss';
 
 createApp(App).use(store).mount('#app');
